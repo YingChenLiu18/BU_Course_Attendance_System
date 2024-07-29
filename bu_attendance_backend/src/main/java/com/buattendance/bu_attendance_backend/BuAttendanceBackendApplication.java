526b0bf570363b3ec4dcb6574c65da08f0eb7ae8
YingChenLiu18 <liuarthuryingchen@gmail.com>
2024-07-29 15:47:14 -0400

    Initial commit

---
+++ bu_attendance_backend/src/main/java/com/buattendance/bu_attendance_backend/BuAttendanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buattendance.bu_attendance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuAttendanceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuAttendanceBackendApplication.class, args);
+    }
+
+}
---
+++ bu_attendance_backend/src/test/java/com/buattendance/bu_attendance_backend/BuAttendanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buattendance.bu_attendance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuAttendanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
